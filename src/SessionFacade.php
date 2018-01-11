8bd72ae0b0c45589582504fc35980faf9ca3a24e
zmingzhi <zmingzhi@yeah.net>
2018-01-11 17:45:18 +0800

    2.0

--- src/SessionFacade.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace zongphp\session;
-use zongphp\framework\build\Facade;
-
-class SessionFacade extends Facade {
-	public static function getFacadeAccessor() {
-		return 'Session';
-	}
-}
